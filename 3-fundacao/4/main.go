c3065c4053866b999122159704db4600cba99d1f
Diego Ramos <diegorbrito9@gmail.com>
2023-10-22 17:35:37 -0300

    [go expert] [3 fundação] fim da aula 'importanto fmt e tipagem'

---
+++ 3-fundacao/4/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+
+	fmt.Printf("o valor da variável 'a' é: %v", a)  // '%v' retorna o valor da variável
+	fmt.Printf("\no tipo da variável 'a' é: %T", a) // '%T' retorna o tipo da variável
+}
